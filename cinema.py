ba55bfc5432401bd670b86db873168aea5cd9893
unknown <mdmahim924214@gmail.com>
2024-04-23 12:59:43 +0600

    done some update

--- cinema.py
+++ cinema.py
@@ -1,43 +1,39 @@
 
 class Star_Cinema:
 
-    hall_list = []
+    __hall_list = []
 
     def entry_hall(self, hall_obj):
-        self.hall_list.append(hall_obj)
+        self.__hall_list.append(hall_obj)
 
-    def show_hall(self):
-        for cHall in self.hall_list:
-            print(f'\nRows: {cHall.rows}, Cols: {cHall.cols}, Hall Number: {cHall.hall_no}')
 
 
 class Hall(Star_Cinema):
 
     def __init__(self, rows, cols, hall_no) -> None:
         super().__init__()
-        self.seats = {}
-        self.show_list = []
+        self.__seats = {}
+        self._show_list = []
         self.rows = rows
         self.cols = cols
-        self.hall_no = hall_no
+        self.__hall_no = hall_no
 
     def entry_show(self, id, movie_name, time):
         movie = (id, movie_name, time)
-        self.show_list.append(movie)
+        self._show_list.append(movie)
         seat = []
         for i in range(self.rows+1):
             row = []
             for j in range(self.cols+1):
                 row.append(0)
             seat.append(row)
-        self.seats[id] = seat
+        self.__seats[id] = seat
 
     def book_seats(self, id, row, col):
-        if id in self.seats:
-            seat = self.seats[id]
+        if id in self.__seats:
             if 0 <= row <= self.rows and 0 <= col <= self.cols:
-                if seat[row][col] == 0:
-                    seat[row][col] = 1
+                if self.__seats[id][row][col] == 0:
+                    self.__seats[id][row][col] = 1
                     print(f"\n\tSeat {row, col} booked successfully !\n")
                 else:
                     print('\nThis seat booked already !')
@@ -47,22 +43,22 @@ class Hall(Star_Cinema):
             print("\nThis Show dose not exisit !")
 
     def view_show_list(self):
-        for list in self.show_list:
+        for list in self._show_list:
             print(f"ID: {list[0]}, Movie: {list[1]}, Time: {list[2]}")
 
     def view_available_seat(self, id):
-        if id in self.seats:
+        if id in self.__seats:
             print(
                 f"------------------Available seats For movie Id : {id}------------------")
             for i in range(self.rows+1):
                 for j in range(self.cols+1):
-                    if self.seats[id][i][j] == 0:
+                    if self.__seats[id][i][j] == 0:
                         print(f"Seat: ({i}, {j})")
 
             print(f"\nUpdated sit matrix: {id}\n")
             for i in range(self.rows+1):
                 for j in range(self.cols+1):
-                    if self.seats[id][i][j] == 0:
+                    if self.__seats[id][i][j] == 0:
                         print("0", end=" ")
                     else:
                         print("1", end=" ")
